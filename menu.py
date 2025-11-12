0bd43552176c84e973648091a00ebef0bfac4dd7
Reuven Lerner <reuven@lerner.co.il>
2025-11-12 21:28:37 +0200

    gitautosync update 81

--- menu.py
+++ menu.py
@@ -12,4 +12,9 @@ def menu(choices):
         print(f'{user_choice} is not valid; try again')
 
 if __name__ == '__main__':
+    # this block will not run when menu.py is imported
+    # this block *will* run when menu.py is executed from the command line as a standalone program
+
+    user_choice = menu(['a', 'b', 'c'])
+    print(f'You chose {user_choice}')
     

68aaad9e8c73448575cb0ed28c6e7d1e2b93b72f
Reuven Lerner <reuven@lerner.co.il>
2025-11-12 20:52:54 +0200

    gitautosync update 64

--- mymod.py
+++ mymod.py
@@ -1,4 +1,4 @@
-print('Now entering mymod!')
+print(f'Now entering {__name__}!')
 
 x = 100
 
@@ -7,4 +7,4 @@ y = [10, 20, 30]
 def hello(name):
     return f'Hello, {name}, from mymod!'
 
-print('Now leaving mymod!')
+print(f'Now leaving {__name__}!')
